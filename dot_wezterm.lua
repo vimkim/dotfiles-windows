b137046e58ce1076adcb12548c9fb49f437ba80a
Daehyun Kim <kimdhyungg@gmail.com>
2024-09-27 12:01:58 +0900

    wezterm(mocha): change theme to mocha

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -7,13 +7,13 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "catppuccin-macchiato"
+config.color_scheme = "Catppuccin Mocha"
 
 local mux = wezterm.mux
 
 wezterm.on("gui-startup", function()
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
 end)
 
 config.wsl_domains = {
@@ -26,54 +26,56 @@ config.wsl_domains = {
 	},
 }
 
-config.font = wezterm.font("JetBrainsMono Nerd Font Propo", {weight = "Bold"})
+-- config.font = wezterm.font("JetBrainsMonoNL Nerd Font Propo", { weight = "Bold" })
+config.font = wezterm.font("NotoSansM Nerd Font Propo", { weight = "Bold" })
 config.font_size = 15.0
 
 config.enable_tab_bar = false
 
 config.default_prog = { "wsl.exe", "--cd", "~" }
 
-
 -- randomly select background images
 
 -- Function to get a random file from the list
 local function get_random_file(files)
-  if #files == 0 then
-    return nil
-
-  end
-  local index = math.random(1, #files)
-  return files[index]
+	if #files == 0 then
+		return nil
+	end
+	local index = math.random(1, #files)
+	return files[index]
 end
 
 -- Get the list of files and select one randomly
 
 -- C:\Users\dhkim\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
-local bg_images_dir_path = wezterm.config_dir ..'/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets'
+local bg_images_dir_path = wezterm.config_dir
+	.. "/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets"
 local files = wezterm.read_dir(bg_images_dir_path)
 local random_file = get_random_file(files)
 
 config.background = {
-    {
-      source = {
-        File = random_file
-      },
-      -- Adjust brightness (lower value makes it dimmer)
-      hsb = {
-        brightness = 0.1, -- Adjust this value to make the background dim
-      },
-    },
+	{
+		source = {
+			File = random_file,
+		},
+		-- Adjust brightness (lower value makes it dimmer)
+		hsb = {
+			brightness = 0.015, -- Adjust this value to make the background dim
+		},
+		-- width = "100%",
+		height = "100%",
+	},
 }
 
 local launch_menu = {}
 
 table.insert(launch_menu, {
-  label = 'PowerShell',
-  args = { 'powershell.exe', '-NoLogo' },
+	label = "PowerShell",
+	args = { "powershell.exe", "-NoLogo" },
 })
 table.insert(launch_menu, {
-  label = 'Pwsh',
-  args = { 'pwsh-preview.exe', '-NoLogo' },
+	label = "Pwsh",
+	args = { "pwsh-preview.exe", "-NoLogo" },
 })
 
 config.launch_menu = launch_menu
